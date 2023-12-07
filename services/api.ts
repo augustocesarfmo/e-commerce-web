843523cbbd6a4f43fe6eaa311fc47c764a50f2b7
Giselle <gisellejacinto1219@gmail.com>
2023-12-07 15:41:46 -0300

    axios post

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://192.168.68.154:3000",
-  timeout: 1000,
-});
+    baseURL: 'http://192.168.68.154:3000/',
+    timeout: 1000,
+  });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,55 @@
 "use client";
-import { useState } from "react";
-import { api } from "../../services/api";
+import {useEffect, useState} from "react"
 
-const initialItens = [
-  { id: 1, nome: "Banana" },
-  { id: 2, nome: "Uva" },
-];
+import { api } from "../../services/api";
+import { Console } from "console";
+
+interface Product {
+  id: number,
+  nome: string;
+}
+const initialItens: Product[] = [];
 
 export default function Home() {
   const [textInput, setTextInput] = useState("");
-  const [itens, setItens] = useState(initialItens);
+  const [itens, setItens] = useState <Product[]>([]);
 
-  async function handleClick() {
+
+  //quando a tela for carregada execute 
+  useEffect(() => {
+    loadProducts();
+  }, [])
+
+  useEffect(() => {
+    console.log("Mudança",textInput);
+  }, [textInput]);
+  async function loadProducts() { //o axios quem criou
     const response = await api.get("/produtos");
-    console.log(response);
+    console.log("Sucess:", response);
     setItens(response.data);
 
     // const response = await fetch("http://192.168.68.154:3000/produtos");
-    // const produtos = await response.json();
+    // const produtos = await response.json(); esse código faz a mesma coisa do de cima que é um a biblioteca axios
     // console.log(produtos);
   }
 
   async function handleAddItem() {
-    // console.log(textInput);
     const data = { nome: textInput };
 
+
     try {
+      const response = await api.post("/produtos", data);
+      loadProducts();
+      console.log("Sucess:", response);
+      
+    } catch (error) {
+      console.log("Error:", error);
+      alert("ocorreu um erro ao tentar se conectar com o servidor")
+    }
+      
+    
+
+    /**try {
       const response = await fetch("http://192.168.68.154:3000/produtos", {
         method: "POST", // or 'PUT'
         headers: {
@@ -39,7 +63,7 @@ export default function Home() {
     } catch (error) {
       console.error("Error:", error);
       alert("Ocorreu um erro");
-    }
+    }**/
   }
 
   return (
@@ -49,10 +73,10 @@ export default function Home() {
           onChange={(e) => setTextInput(e.target.value)}
           placeholder="Digite o seu texto aqui..."
         />
+       
+        
         <button onClick={handleAddItem}>Enviar</button>
       </div>
-
-      <button onClick={handleClick}>Buscar informação no servidor</button>
 
       <ul>
         {itens.map((item) => (
